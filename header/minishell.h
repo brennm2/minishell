60dac339d2dbe8c468c8c93d12c5ff8ab69a7a2e
nicoleoliveiraa <nicole.s.oliveira26@gmail.com>
2024-05-27 18:59:03 +0100

    expand - checking

--- header/minishell.h
+++ header/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nsouza-o <nsouza-o@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:46:56 by bde-souz          #+#    #+#             */
-/*   Updated: 2024/05/27 16:02:06 by nsouza-o         ###   ########.fr       */
+/*   Updated: 2024/05/27 17:07:56 by nsouza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ typedef enum s_types
 	append, //ok
 	expander,
 	is_pipe,
+	not_expander,
 }				t_types;
 
 
--- src/parsing/expand.c
+++ src/parsing/expand.c
@@ -6,22 +6,50 @@
 /*   By: nsouza-o <nsouza-o@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 15:29:01 by nsouza-o          #+#    #+#             */
-/*   Updated: 2024/05/27 15:45:44 by nsouza-o         ###   ########.fr       */
+/*   Updated: 2024/05/27 17:48:42 by nsouza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/minishell.h"
 
-void	is_expand(t_token *token, t_data *data)
+/* void	is_expand(t_token *token, t_data *data) */
+
+int	ft_isupper(int c)
 {
-	bool	single_quotes;
-	bool	doble_quotes;
-	int		i;
+	if ((c >= 65 && c <= 90))
+		return (1);
+	return (0);
+}
+
+void	is_expand(char *token)
+{
+	int	i;
+	int	j;
 
 	i = -1;
-	while (token->str[++i])
+	/* if (token->type == not_expander)
+		return ; */
+	while (token[++i])
 	{
-		if (token->str[i] == '"')
-			
+		if (token[i] == '$')
+		{
+			j = i;
+			if (token[i + 1] == '?')
+				printf("error\n");
+			while (ft_isupper(token[++i]))
+				;
+			printf("%d, %d\n", j, (i - 1));
+		}
 	}
+}
+
+int main(int ac, char **av, char **envp)
+{
+	char *token;
+	//t_data *data;
+	
+	token = "aaaa$aUSERRRaaa'";
+	//get_env(&data, envp);
+	is_expand(token);
+	return (0);
 }
