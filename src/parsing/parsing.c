0eb139d27220820a0b1ae01fec4377fb970b71b0
nicoleoliveiraa <nicole.s.oliveira26@gmail.com>
2024-06-11 16:35:17 +0100

    for merge

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: nsouza-o <nsouza-o@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 11:57:06 by bde-souz          #+#    #+#             */
-/*   Updated: 2024/06/07 17:07:15 by nsouza-o         ###   ########.fr       */
+/*   Updated: 2024/06/11 16:16:44 by nsouza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	save_substring(char *buffer, int start, int end, t_data *data)
 
 	i = 0;
 	while(start <= end)
+	{
 		data->token->str[i++] = buffer[start++];
-	if(buffer[start] == D_QUOTES || buffer[start] == S_QUOTES)
-		start++;
+	}
 	if(buffer[start])
 	{
 		data->token = data->token->next;
@@ -47,7 +47,7 @@ void	get_split(char *buffer, t_data *data)
 {
 	t_token	*reset_index;
 	int		i;
-
+	
 	reset_index = data->token;
 	i = 0;
 	while(buffer[i])
@@ -56,14 +56,18 @@ void	get_split(char *buffer, t_data *data)
 		if (buffer[i] && (buffer[i] >= 7 && buffer[i] <= 32))
 		{
 			i = move_space(buffer, i);
-			save_space(buffer, i, data);
+			//save_space(buffer, i, data);
 		}
-		if(buffer[i] == D_QUOTES)
-				i = move_with_quotes(buffer, i, D_QUOTES, data);
-		if (buffer[i] == S_QUOTES)
-				i = move_with_quotes(buffer, i, S_QUOTES, data);
+		
+		// if(buffer[i] == D_QUOTES)
+		// dquotes = 1;
+		// 		i = move_with_quotes(buffer, i, D_QUOTES, data);
+		// if (buffer[i] == S_QUOTES)
+		// 		i = move_with_quotes(buffer, i, S_QUOTES, data);
 		if (buffer[i] && !(buffer[i] >= 7 && buffer[i] <= 32))
-				i = move_without_quotes(buffer, i, data);
+		{
+				i = move_without_quotes(buffer, i, data);		
+		}
 	}
 	data->token = reset_index;
 }
